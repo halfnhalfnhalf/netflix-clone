fd2038e17ceab14b89a6b19071ba47005171c905
Donovan Butcher <halfhalfnhalf@gmail.com>
2026-08-18 09:13:58 -0600

    feat: add movie api data retrieval

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,35 @@
 import Banner from "@/components/Banner";
-import Header from "@/components/Header"
+import Header from "@/components/Header";
+import requests from "@/utils/requests";
 
-export default function Home() {
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+}
+
+export default async function Home() {
+  const {
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  } = await getData();
+
   return (
     <div className="relative h-screen bg-linear-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Header />
       <main>
-        <Banner />
+        <Banner netflixOriginals={netflixOriginals} />
         <section>
           {/* row */}
           {/* row */}
@@ -20,4 +43,53 @@ export default function Home() {
       {/* modal */}
     </div>
   );
+}
+
+async function getData() {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals, { cache: "no-store" }).then((res) =>
+      res.json(),
+    ),
+    fetch(requests.fetchTrending, { cache: "no-store" }).then((res) =>
+      res.json(),
+    ),
+    fetch(requests.fetchTopRated, { cache: "no-store" }).then((res) =>
+      res.json(),
+    ),
+    fetch(requests.fetchActionMovies, { cache: "no-store" }).then((res) =>
+      res.json(),
+    ),
+    fetch(requests.fetchComedyMovies, { cache: "no-store" }).then((res) =>
+      res.json(),
+    ),
+    fetch(requests.fetchHorrorMovies, { cache: "no-store" }).then((res) =>
+      res.json(),
+    ),
+    fetch(requests.fetchRomanceMovies, { cache: "no-store" }).then((res) =>
+      res.json(),
+    ),
+    fetch(requests.fetchDocumentaries, { cache: "no-store" }).then((res) =>
+      res.json(),
+    ),
+  ]);
+
+  return {
+    netflixOriginals: netflixOriginals.results,
+    trendingNow: trendingNow.results,
+    topRated: topRated.results,
+    actionMovies: actionMovies.results,
+    comedyMovies: comedyMovies.results,
+    horrorMovies: horrorMovies.results,
+    romanceMovies: romanceMovies.results,
+    documentaries: documentaries.results,
+  };
 }
