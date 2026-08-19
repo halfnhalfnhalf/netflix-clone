e134387719a3f5e76ef17f6bf6f321f3f99b1620
Donovan Butcher <halfhalfnhalf@gmail.com>
2026-08-19 13:04:51 -0600

    feat: change banner and main layout styling a bit

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home() {
   return (
     <div className="relative h-screen bg-linear-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Header />
-      <main>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
         <section>
           {/* row */}
