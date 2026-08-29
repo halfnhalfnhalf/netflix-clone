e2500e4b1e48a4ac782700a147d29eeb2b34ad25
Donovan Butcher <halfhalfnhalf@gmail.com>
2026-08-28 19:57:45 -0600

    feat: start adding account page

---
+++ app/account/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import Membership from "@/components/Membership";
+import useAuth from "@/hooks/useAuth";
+import useSubscription from "@/hooks/useSubscription";
+import { fetchActiveProducts } from "@/lib/stripe";
+import { Product } from "@invertase/firestore-stripe-payments";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+function Account() {
+  const { user, logout } = useAuth();
+  const subscription = useSubscription(user);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getProduct = () => {
+    if (products) {
+      return products.filter(
+        (product) => product.id === subscription?.product,
+      )[0]?.name;
+    }
+  };
+
+  useEffect(() => {
+    fetchActiveProducts().then((res) => {
+      if (res) setProducts(res);
+    });
+  }, []);
+
+  return (
+    <div>
+      <title>Account Settings - Netflix Clone</title>
+
+      <header className={`bg-[#141414]`}>
+        <Link href="/">
+          <img
+            src="https://rb.gy/ulxxee"
+            width={120}
+            height={120}
+            className="cursor-pointer object-contain"
+          />
+        </Link>
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </header>
+
+      <main className="pt-24 mx-auto max-w-6xl px-5 pb-12 transition-all md:px-10">
+        <div className="flex flex-col gap-x-4 md:flex-row md:items-center">
+          <h1 className="text-3xl md:text-4xl">Account</h1>
+          <div className="-ml-0.5 flex items-center gap-x-1.5">
+            <img src="https://rb.gy/4vfk4r" alt="" className="h-7 w-7" />
+            <p className="text-xs font-semibold text-[#555]">
+              Member since {subscription?.created}
+            </p>
+          </div>
+        </div>
+
+        <Membership />
+
+        <div className="mt-6 grid grid-cols-1 gap-x-4 border p-4 md:grid-cols-4 md:border-x-0 md:border-b-0 md:px-0 md:pb-0">
+          <h4>Plan Details</h4>
+          <div>{getProduct()}</div>
+          <p className="cursor-pointer text-blue-500 hover:underline md:text-right">Change plan</p>
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0">
+          <h4 className="text-lg text-[gray]">Settings</h4>
+          <p
+            className="col-span-3 cursor-pointer text-blue-500 hover:underline"
+            onClick={logout}
+          >
+            Sign out of all devices
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Account;
